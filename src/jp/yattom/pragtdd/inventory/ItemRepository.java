48376ee101a9ec3e3512789c37fb7411045fac3d
yattom <tsutomu.yasui@gmail.com>
2012-07-07 14:51:50 +0900

    すべての商品を取得できるようにした

--- src/jp/yattom/pragtdd/inventory/ItemRepository.java
+++ src/jp/yattom/pragtdd/inventory/ItemRepository.java
@@ -1,5 +1,6 @@
 package jp.yattom.pragtdd.inventory;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,14 +18,14 @@ public class ItemRepository {
     }
 
     public void store(Item item) throws InventoryException {
-        if(repository.containsKey(item.getName())) {
+        if (repository.containsKey(item.getName())) {
             throw new InventoryException("指定された名前の商品はすでに存在します");
         }
         repository.put(item.getName(), item);
     }
 
     public Item findByName(String name) throws InventoryException {
-        if(!repository.containsKey(name)) {
+        if (!repository.containsKey(name)) {
             throw new InventoryException("指定された名前の商品は存在しません");
         }
         return repository.get(name);
@@ -33,4 +34,9 @@ public class ItemRepository {
     public void clear() {
         repository.clear();
     }
+
+    public Collection<Item> allItems() {
+        return repository.values();
+    }
+
 }
--- src/jp/yattom/pragtdd/inventory/ItemRepositoryTest.java
+++ src/jp/yattom/pragtdd/inventory/ItemRepositoryTest.java
@@ -2,6 +2,7 @@ package jp.yattom.pragtdd.inventory;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.hasItems;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -22,16 +23,41 @@ public class ItemRepositoryTest {
         assertThat(repo.findByName("商品A"), is(item));
     }
 
-    @Test(expected=InventoryException.class)
+    @Test(expected = InventoryException.class)
     public void Itemを保存して取り出せる_存在しなければ例外() throws Exception {
         repo.findByName("存在しない商品");
     }
 
-    @Test(expected=InventoryException.class)
+    @Test(expected = InventoryException.class)
     public void Itemを保存して取り出せる_同じ名前で二度保存できない() throws Exception {
         Item item1 = new Item("商品A");
         Item item2 = new Item("商品A");
         repo.store(item1);
         repo.store(item2);
+    }
+
+    @Test
+    public void Itemの一覧を取得できる_空の場合() throws Exception {
+        assertThat(repo.allItems().size(), is(0));
+    }
+
+    @Test
+    public void Itemの一覧を取得できる_1件の場合() throws Exception {
+        Item item = new Item("商品A");
+        repo.store(item);
+        assertThat(repo.allItems().size(), is(1));
+        assertThat(repo.allItems(), hasItems(item));
+    }
+
+    @Test
+    public void Itemの一覧を取得できる_複数件の場合() throws Exception {
+        Item itemA = new Item("商品A");
+        Item itemB = new Item("商品B");
+        Item itemC = new Item("商品C");
+        repo.store(itemA);
+        repo.store(itemB);
+        repo.store(itemC);
+        assertThat(repo.allItems().size(), is(3));
+        assertThat(repo.allItems(), hasItems(itemA, itemB, itemC));
     }
 }
