0a51087e40b22490e1617ded4066ea37c251b667
yattom <tsutomu.yasui@gmail.com>
2012-07-07 10:27:38 +0900

    テストをリファクタリング(続)

--- src/jp/yattom/pragtdd/inventory/ItemTest.java
+++ src/jp/yattom/pragtdd/inventory/ItemTest.java
@@ -2,27 +2,30 @@ package jp.yattom.pragtdd.inventory;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class ItemTest {
+    private Item item;
+    @Before
+    public void itemを準備() {
+        item = new Item();
+    }
+
     @Test
     public void 在庫を取得できる_最初はゼロ() throws Exception {
-        Item item = new Item();
         assertThat(item.getStock(), is(0));
     }
 
     @Test
     public void 在庫を取得できる_増やして取得() throws Exception {
-        Item item = new Item();
         item.addStock(10);
         assertThat(item.getStock(), is(10));
     }
 
     @Test
     public void 在庫を増やす_連続() throws Exception {
-        Item item = new Item();
         item.addStock(10);
         item.addStock(5);
         assertThat(item.getStock(), is(15));
@@ -30,7 +33,6 @@ public class ItemTest {
 
     @Test(expected = InventoryException.class)
     public void 在庫を増やす_ゼロは許さない() throws Exception {
-        Item item = new Item();
         item.addStock(0);
     }
 }
