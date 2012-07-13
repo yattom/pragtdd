683be4796d72fcd918d2bef11ad07947e1962a51
yattom <tsutomu.yasui@gmail.com>
2012-07-13 09:38:20 +0900

    RunWith(Enclosed.class)を試してみた

--- src/jp/yattom/pragtdd/inventory/ItemRepositoryTest.java
+++ src/jp/yattom/pragtdd/inventory/ItemRepositoryTest.java
@@ -6,58 +6,73 @@ import static org.junit.matchers.JUnitMatchers.hasItems;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
 
+@RunWith(Enclosed.class)
 public class ItemRepositoryTest {
-    private ItemRepository repo;
+	static public class Itemを保存して取り出せる {
+		private ItemRepository repo;
 
-    @Before
-    public void ItemManagerの準備() throws Exception {
-        repo = ItemRepository.getInstance();
-        repo.clear();
-    }
+		@Before
+		public void ItemManagerの準備() throws Exception {
+			repo = ItemRepository.getInstance();
+			repo.clear();
+		}
 
-    @Test
-    public void Itemを保存して取り出せる() throws Exception {
-        Item item = new Item("商品A");
-        repo.store(item);
-        assertThat(repo.findByName("商品A"), is(item));
-    }
+		@Test
+		public void 通常() throws Exception {
+			Item item = new Item("商品A");
+			repo.store(item);
+			assertThat(repo.findByName("商品A"), is(item));
+		}
 
-    @Test(expected = InventoryException.class)
-    public void Itemを保存して取り出せる_存在しなければ例外() throws Exception {
-        repo.findByName("存在しない商品");
-    }
+		@Test(expected = InventoryException.class)
+		public void Itemを保存して取り出せる_存在しなければ例外() throws Exception {
+			repo.findByName("存在しない商品");
+		}
 
-    @Test(expected = InventoryException.class)
-    public void Itemを保存して取り出せる_同じ名前で二度保存できない() throws Exception {
-        Item item1 = new Item("商品A");
-        Item item2 = new Item("商品A");
-        repo.store(item1);
-        repo.store(item2);
-    }
+		@Test(expected = InventoryException.class)
+		public void Itemを保存して取り出せる_同じ名前で二度保存できない() throws Exception {
+			Item item1 = new Item("商品A");
+			Item item2 = new Item("商品A");
+			repo.store(item1);
+			repo.store(item2);
+		}
+	}
 
-    @Test
-    public void Itemの一覧を取得できる_空の場合() throws Exception {
-        assertThat(repo.allItems().size(), is(0));
-    }
+	static public class Itemの一覧を取得できる {
+		private ItemRepository repo;
 
-    @Test
-    public void Itemの一覧を取得できる_1件の場合() throws Exception {
-        Item item = new Item("商品A");
-        repo.store(item);
-        assertThat(repo.allItems().size(), is(1));
-        assertThat(repo.allItems(), hasItems(item));
-    }
+		@Before
+		public void ItemManagerの準備() throws Exception {
+			repo = ItemRepository.getInstance();
+			repo.clear();
+		}
 
-    @Test
-    public void Itemの一覧を取得できる_複数件の場合() throws Exception {
-        Item itemA = new Item("商品A");
-        Item itemB = new Item("商品B");
-        Item itemC = new Item("商品C");
-        repo.store(itemA);
-        repo.store(itemB);
-        repo.store(itemC);
-        assertThat(repo.allItems().size(), is(3));
-        assertThat(repo.allItems(), hasItems(itemA, itemB, itemC));
-    }
+		@Test
+		public void Itemの一覧を取得できる_空の場合() throws Exception {
+			assertThat(repo.allItems().size(), is(0));
+		}
+
+		@Test
+		public void Itemの一覧を取得できる_1件の場合() throws Exception {
+			Item item = new Item("商品A");
+			repo.store(item);
+			assertThat(repo.allItems().size(), is(1));
+			assertThat(repo.allItems(), hasItems(item));
+		}
+
+		@Test
+		public void Itemの一覧を取得できる_複数件の場合() throws Exception {
+			Item itemA = new Item("商品A");
+			Item itemB = new Item("商品B");
+			Item itemC = new Item("商品C");
+			repo.store(itemA);
+			repo.store(itemB);
+			repo.store(itemC);
+			assertThat(repo.allItems().size(), is(3));
+			assertThat(repo.allItems(), hasItems(itemA, itemB, itemC));
+		}
+	}
 }
