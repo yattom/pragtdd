96e940d35f3e30d7eb52eb05c6954d93d50432d5
yattom <tsutomu.yasui@gmail.com>
2012-07-16 16:39:43 +0900

    テスト用初期データを入れた。

--- src/jp/yattom/pragtdd/inventory/web/ItemResource.java
+++ src/jp/yattom/pragtdd/inventory/web/ItemResource.java
@@ -4,11 +4,21 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+import jp.yattom.pragtdd.inventory.InventoryException;
 import jp.yattom.pragtdd.inventory.Item;
 import jp.yattom.pragtdd.inventory.ItemRepository;
 
 @Path("/items")
 public class ItemResource {
+    static {
+        try {
+            ItemRepository.getInstance().store(new Item("商品A"));
+            ItemRepository.getInstance().store(new Item("商品B"));
+            ItemRepository.getInstance().store(new Item("商品C"));
+        } catch (InventoryException e) {
+            throw new RuntimeException("テスト用商品データが作成できませんでした", e);
+        }
+    }
     @GET
     @Produces("application/xml")
     public String allItems() {
