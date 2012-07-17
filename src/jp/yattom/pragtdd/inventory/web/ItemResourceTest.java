f25c58a6c8d367f358696a67fa0c8b3b83a9d5fc
yattom <tsutomu.yasui@gmail.com>
2012-07-17 17:28:51 +0900

    APIで商品を登録するテストを書いた

--- src/jp/yattom/pragtdd/inventory/web/ItemResourceTest.java
+++ src/jp/yattom/pragtdd/inventory/web/ItemResourceTest.java
@@ -2,15 +2,16 @@ package jp.yattom.pragtdd.inventory.web;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 import java.io.ByteArrayInputStream;
 
+import jp.yattom.pragtdd.inventory.InventoryException;
 import jp.yattom.pragtdd.inventory.ItemRepository;
 
 import org.junit.Test;
 
 public class ItemResourceTest {
-
     @Test
     public void 商品を新規作成できる() throws Exception {
         StringBuilder req = new StringBuilder();
@@ -25,4 +26,22 @@ public class ItemResourceTest {
                 is("商品X"));
     }
 
+    @Test
+    public void 商品を新規作成できる_同じ名前はエラー() throws Exception {
+        StringBuilder req = new StringBuilder();
+        req.append("<item>");
+        req.append("<name>商品X</name>");
+        req.append("</item>");
+        ByteArrayInputStream is = new ByteArrayInputStream(req.toString()
+                .getBytes("utf-8"));
+        new ItemResource().createItem(is);
+
+        try {
+            is.reset();
+            new ItemResource().createItem(is);
+            fail("同じ名前では作成できない");
+        } catch (InventoryException e) {
+            // ok
+        }
+    }
 }
