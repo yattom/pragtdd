99f2b144ea362c2eafedf745403f0f65d01821c4
yattom <tsutomu.yasui@gmail.com>
2012-07-29 23:09:17 +0900

    APIを叩くテストを実装中。APIをアクセスするメソッドを書いている。

--- src/jp/yattom/pragtdd/inventory/web/APITest.java
+++ src/jp/yattom/pragtdd/inventory/web/APITest.java
@@ -3,7 +3,21 @@ package jp.yattom.pragtdd.inventory.web;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import jp.yattom.pragtdd.inventory.web.test.ItemResourceUtil;
+
 import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 public class APITest {
 
@@ -38,18 +52,42 @@ public class APITest {
         return 0;
     }
 
-    private boolean 商品が存在する(String name) {
-        // TODO Auto-generated method stub
+    private boolean 商品が存在する(String name) throws Exception {
+        URL url = new URL("http://localhost:8080/pragtdd-spike/items/");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        InputStream is = (InputStream) conn.getContent();
+        DocumentBuilder builder = DocumentBuilderFactory.newInstance()
+                .newDocumentBuilder();
+        Document doc = builder.parse(is);
+        NodeList items = doc.getElementsByTagName("item");
+        for (int i = 0; i < items.getLength(); i++) {
+            Element element = (Element) items.item(i);
+            NodeList nameElement = element.getElementsByTagName("name");
+            String nameOfItem = ((Element) nameElement.item(0))
+                    .getTextContent();
+            if (nameOfItem.equals(name)) {
+                return true;
+            }
+        }
         return false;
     }
 
-    private void 商品を作成する(String name) {
-        // TODO Auto-generated method stub
-
+    private void 商品を作成する(String name) throws Exception {
+        URL url = new URL("http://localhost:8080/pragtdd-spike/items/");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setDoOutput(true);
+        conn.setRequestProperty("Content-type", "application/xml");
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(
+                conn.getOutputStream(), "utf-8"));
+        out.print(ItemResourceUtil.itemXml(name));
+        out.flush();
+        conn.getInputStream();
+        assertThat(conn.getResponseCode(), is(HttpURLConnection.HTTP_NO_CONTENT));
     }
 
-    private boolean 商品が存在しない(String name) {
-        // TODO Auto-generated method stub
-        return false;
+    private boolean 商品が存在しない(String name) throws Exception {
+        return !商品が存在する(name);
     }
+
 }
